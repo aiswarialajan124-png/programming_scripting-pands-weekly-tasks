285f653d1483ca8d71b217898abbcf2ed72f2269
lajan <aiswarialajan124@gmail.com>
2026-03-23 15:12:47 +0000

    Weekly Task 05 - completed weekday.py

--- weekday.py
+++ weekday.py
@@ -3,7 +3,7 @@ import datetime
 # weekday() returns a number , Monday = 0 ... Sunday = 6
 today = datetime.datetime.today().weekday()
 
-# If number is less than 5, it is Monday-Friday (a weekday)
+# If number is less than 5, it is Monday-Friday (a weekday) else its a weekend
 if today < 5:
     print("Today is a weekday")
 else:
